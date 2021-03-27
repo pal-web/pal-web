84edf85d41612a2c57d06df556d15cbf438a1a63
pal-web <80841133+pal-web@users.noreply.github.com>
2021-03-27 21:23:47 +0530

    Add files via upload

---
+++ alag.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void great(int a,int b)
+{
+if(a>b)
+printf("\n %d is greater than %d",a,b);
+else
+printf("\n %d is greater than %d",b,a);
+}
+void main()
+{
+    int a,b;
+clrscr()_;
+printf("enter two nunber:\n") ;
+scanf("%d%d",&a,&b);
+great(a,b);
+getch();
+}hhhhh
