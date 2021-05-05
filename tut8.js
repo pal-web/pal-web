fb4f4fc6f279a67ac478986629416a5d1881f0c7
palak dubey <80841133+pal-web@users.noreply.github.com>
2021-05-05 23:37:46 +0530

    Add files via upload

---
+++ tut8.js
@@ -0,0 +1,48 @@
+console.log('this is tut8');
+const age = 18;
+const vari = 'undefined';
+//const doesdrive ='false';
+
+
+if(age==18){
+    console.log('age is 18')
+}
+else if(age===96){
+    console.log('age is 65')
+}
+else{
+    console.log('age is not 18')
+}
+if(typeof vari !== 'undefined'){
+    console.log( 'vari is defined');
+
+}
+else{
+
+
+    console.log('vari is not defined');
+}
+/*
+if(doesdrive ||  age=14){
+    console.log("you can drive");
+}
+else{
+    console.log("you can not drive");
+}*/
+
+/*
+switch (age) {
+    case 18:
+        console.log("you are 18");
+        
+        break;
+        case 24:
+            console.log("you are 28");
+            break;
+
+    default:
+        ("you are unknown age");
+        break;
+}*/
+
+
