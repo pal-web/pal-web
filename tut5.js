53c6e231dba8bb204a76bbc72661fd97df9eef56
palak dubey <80841133+pal-web@users.noreply.github.com>
2021-05-05 23:39:04 +0530

    Add files via upload

---
+++ tut5.js
@@ -0,0 +1,33 @@
+console.log('welcome to my tut5');
+let myvar = 43
+let myvar = string(34);
+
+let booleanvar = string(true);
+console.log(booleanvar, (typeof booleanvar));
+
+let arr = string([1,2,3,4,5,6]);
+console.log(aee.length, (typeof arr));
+
+let i =54;
+console.log(i.toString())
+
+let stri =number("3434");
+stri = ("2433");
+stri =("true");
+stri=("1,2,3,4,5,6,7");
+console.log(stri,(typeof stri));
+
+let i =false();
+console.log(i . toString())
+
+let number = parsefloat("34.098");
+
+console,log(number.tofixed(5), (typeof number))
+;
+
+//type coercion
+let mystr = number ("657");
+let mynum =45;
+
+console.log(mystr + mynum);
+
