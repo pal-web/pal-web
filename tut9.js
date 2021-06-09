e7d462f2b47d9d4f2fefedbb96f8bafaf770151f
palak dubey <80841133+pal-web@users.noreply.github.com>
2021-06-09 23:19:11 +0530

    Add files via upload

---
+++ tut9.js
@@ -0,0 +1,38 @@
+console.log("this is tut9");
+/*for(let i=0; i<100; i++)
+{
+    console.log(i);
+}*/
+
+
+let j=o;
+while(j<19){
+    console.log(j+1);
+    j+=1;
+}
+
+let k =  0;
+do{
+    if(k==5){
+        break;
+    }
+    console.log(k+1);
+    k+=1;
+
+}while(k < 10);
+console.log("done");
+/*
+let arr =[1,2,3,4,5];
+arr.forEach(function(element){
+    console.log(element);
+
+});
+  let obj = {
+      name:'harry',
+      channel:'cwh'
+  }
+  for(let key in obj){
+      console.log(`the ${ket} of object is ${obj[key]}`)
+  }*/
+
+  
